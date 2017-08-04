cf0a3335f40995a0ae317b28fa57512f77f246ec
Heiko Henning <heiko.henning@mtrail.ch>
2017-08-04 07:01:48 +0200

    ListenDistributedQueue

--- src/ch/mtrail/tibrv/playground/ListenDQ.java
+++ src/ch/mtrail/tibrv/playground/ListenDQ.java
@@ -6,22 +6,21 @@ import java.io.InputStreamReader;
 import java.util.Date;
 
 import com.tibco.tibrv.Tibrv;
+import com.tibco.tibrv.TibrvCmQueueTransport;
 import com.tibco.tibrv.TibrvException;
-import com.tibco.tibrv.TibrvFtMember;
-import com.tibco.tibrv.TibrvFtMemberCallback;
 import com.tibco.tibrv.TibrvListener;
 import com.tibco.tibrv.TibrvMsg;
 import com.tibco.tibrv.TibrvMsgCallback;
+import com.tibco.tibrv.TibrvQueue;
 import com.tibco.tibrv.TibrvRvdTransport;
 
-public class ListenFT implements TibrvMsgCallback {
+public class ListenDQ implements TibrvMsgCallback {
 
 	private boolean performDispatch = true;
-	private final String ftGroupName = "FT_roup_Name";
-	private TibrvFtMember ftMember;
-	private int ftStatus;
+	private final String dqGroupName = "DQgroupName";
+	private TibrvQueue queue;
 
-	public ListenFT(final String service, final String network, final String daemon, final String subject) {
+	public ListenDQ(final String service, final String network, final String daemon, final String subject) {
 
 		// open Tibrv in native implementation
 		try {
@@ -43,49 +42,20 @@ public class ListenFT implements TibrvMsgCallback {
 			e.printStackTrace();
 			System.exit(1);
 		}
-
-		// create listener using default queue
+		
+		// DistributedQueue + listener using default queue
+		TibrvCmQueueTransport dq;
 		try {
-			new TibrvListener(Tibrv.defaultQueue(), this, transport, subject, null);
+			queue = new TibrvQueue();
+            
+			dq = new TibrvCmQueueTransport(transport, dqGroupName);
+			//dq.setWorkerTasks(2);
+
+			new TibrvListener(queue, this, dq, subject, null);
 			System.err.println("Listening on: " + subject);
+			
 		} catch (final TibrvException e) {
-			System.err.println("Failed to create listener:");
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		// FaultTolerance
-		final TibrvFtMemberCallback callback = new TibrvFtMemberCallback() {
-			@Override
-			public void onFtAction(final TibrvFtMember member, final String groupName, final int action) {
-				ftStatus = action;
-				System.err.println(System.currentTimeMillis() + " " + groupName + " -> " + //
-						getFtStatus(action) + " [" + action + "]");
-			}
-		};
-
-		/**
-		 * TibrvFtMember( 
-		 *   TibrvQueue queue, 
-		 *   TibrvFtMemberCallback callback,
-		 *   TibrvTransport transport, 
-		 *   String groupName, 
-		 *   int weight, 
-		 *   int activeGoal, 
-		 *   
-		 *   double heartbeatInterval, 
-		 *   double preparationInterval,
-		 *   double activationInterval, 
-		 *   Object closure 
-		 * )
-		 */
-		try {
-			final int defaultWeight = 50;
-			final int activePrcocesses = 2;
-			ftMember = new TibrvFtMember(Tibrv.defaultQueue(), callback, transport, ftGroupName, defaultWeight, activePrcocesses, //
-					0.5, 0, 1.0, null);
-		} catch (final TibrvException e) {
-			System.err.println("Failed to create FT member:");
+			System.err.println("Error setup distributed queue or listener");
 			e.printStackTrace();
 			System.exit(1);
 		}
@@ -97,7 +67,7 @@ public class ListenFT implements TibrvMsgCallback {
 				// dispatch Tibrv events
 				try {
 					// Wait max 0.5 sec, to listen on keyboard.
-					Tibrv.defaultQueue().timedDispatch(0.5d);
+					queue.timedDispatch(0.5d);
 				} catch (final TibrvException e) {
 					System.err.println("Exception dispatching default queue:");
 					e.printStackTrace();
@@ -119,26 +89,12 @@ public class ListenFT implements TibrvMsgCallback {
 
 	@Override
 	public void onMsg(final TibrvListener listener, final TibrvMsg msg) {
-		System.out.println((new Date()).toString() + " " + getFtStatus(ftStatus) + "(" + ftMember.getWeight() + "): " //
+		System.out.println((new Date()).toString() + " " //
 				+ "subject=" + msg.getSendSubject() + ", reply=" + msg.getReplySubject() //
 				+ ", message=" + msg.toString());
 		System.out.flush();
 
 		msg.dispose();
-	}
-
-	private static String getFtStatus(final int ftStatus) {
-		switch (ftStatus) {
-		case TibrvFtMember.ACTIVATE:
-			return "ACTIVE";
-		case TibrvFtMember.DEACTIVATE:
-			return "DEACTIVE";
-		case TibrvFtMember.PREPARE_TO_ACTIVATE:
-			return "PREPARE_TO_ACTIVATE";
-		case TibrvFtMember.PREPARE_AND_ACTIVATE:
-			return "PREPARE_AND_ACTIVATE";
-		}
-		return "";
 	}
 
 	public boolean isPerformDispatch() {
@@ -157,7 +113,7 @@ public class ListenFT implements TibrvMsgCallback {
 		argParser.setRequiredArg("subject");
 		argParser.parse(args);
 
-		final ListenFT listen = new ListenFT(//
+		final ListenDQ listen = new ListenDQ(//
 				argParser.getParameter("service"), //
 				argParser.getParameter("network"), //
 				argParser.getParameter("daemon"), //
@@ -174,9 +130,9 @@ public class ListenFT implements TibrvMsgCallback {
 		new Thread(() -> {
 			try (BufferedReader input = new BufferedReader(new InputStreamReader(System.in, "UTF-8"))) {
 				while (true) {
-					final String line = input.readLine().trim();
+					final char c = (char) input.read();
 
-					switch (line.charAt(0)) {
+					switch (c) {
 					case 'd':
 					case 'D':
 						System.out.println("Dispatcher is DISABLED");
@@ -188,19 +144,13 @@ public class ListenFT implements TibrvMsgCallback {
 						System.out.println("Dispatcher is ENABLED");
 						setPerformDispatch(true);
 						break;
+						
+					case '\r':
+					case '\n':
+						break;
 
 					default:
-						if (line.matches("^\\d+$")) {
-							try {
-								ftMember.setWeight(Integer.parseInt(line));
-								System.out.println("Set weight to " + line);
-							} catch (TibrvException | NumberFormatException e) {
-								System.err.println("Exception to set ft weight:");
-								e.printStackTrace();
-							}
-						} else {
-							printKeyUsage();
-						}
+						printKeyUsage();
 						break;
 					}
 
